9293f5f2750f354c85b18c988360be51649fff7c
minseoyya <minseoyya@gmail.com>
2026-02-13 23:37:51 +0900

    p
    p

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -54,13 +54,13 @@ export const projects: Project[] = [
             {
         id: 'p1-5',
         type: 'image',
-        url: '/images/p1/01_02.jpg',
+        url: '/images/p1/p1_02.jpg',
         alt: 'Ethereal Bloom - Detail'
       },
       {
         id: 'p1-6',
         type: 'image',
-        url: '/images/p1/01_03.jpg',
+        url: '/images/p1/p1_03.jpg',
         alt: 'Ethereal Bloom - Detail 2'
       },
       {
